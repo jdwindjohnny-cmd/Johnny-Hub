75927d92ecd7c07ed1d390b00dccd6287f80eb72
jdwindjohnny-cmd <jd.windjohnny@gmail.com>
2026-08-17 12:14:02 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const C="my-hub-v1",A=["./","./index.html","./manifest.webmanifest","./icon.svg"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
